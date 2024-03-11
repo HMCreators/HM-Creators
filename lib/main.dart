63f2adc082b3617322a9df2b9934ca0a90ebc70a
HMCreators <info@hmcreatorsllc.com>
2024-03-11 13:00:06 -0400

    Error git commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Scrollable Website',
+      title: 'HM Creators',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
